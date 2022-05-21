d29d56236aafb3c8080bb5fa827bc3469a6656e7
Luís Tchitue <ls04af@gmail.com>
2022-05-21 19:46:44 +0100

    defines the basic logic for the filter controller

--- src/app/reports/auxiliary-components/filter-window/auxiliary-components/quantity-filter-component/quantity-filter-component.component.ts
+++ src/app/reports/auxiliary-components/filter-window/auxiliary-components/quantity-filter-component/quantity-filter-component.component.ts
@@ -8,44 +8,14 @@ import { ServiceFilterMessengerService } from 'src/app/reports/service-messenger
   styleUrls: ['./quantity-filter-component.component.css']
 })
 export class QuantityFilterComponentComponent implements OnInit {
-
-  public transactionDirection! : string;
-  public rangeOperatorAndValue! : string;
   
-  public qtyFilterInputs : Array<{
-      name : string, 
-      value : string, 
-      isActive : boolean, 
-      isDisabled : boolean, 
-      error : boolean,
-      errorMsg : string
-    }> = [
-      {
-        name : 'Quantia Especifica', 
-        value : '', 
-        isActive : false, 
-        isDisabled : false,
-        error : false,
-        errorMsg : ''
-      },
-      {
-        name : 'Pelo menos de...', 
-        value : '', 
-        isActive : false, 
-        isDisabled : false,
-        error : false,
-        errorMsg : ''
-      },
-      {
-        name : 'Não mais que...', 
-        value : '', 
-        isActive : false, 
-        isDisabled : false,
-        error : false,
-        errorMsg : ''
-      }
-  ];
-  
+  public filter : {direction : string, value : string } = {direction : "", value : ""};
+  public isThereAnIntervalError : boolean = false;
+  public interValErrorMsgs : Array<{errorMsg : string}> = [
+    {errorMsg : 'Deve ser menor que \'Não mais que...\''},
+    {errorMsg : 'Deve ser maior que \'Pelo menos até..\''}
+  ] 
+ 
   constructor(
     private serviceFilterMessengerService : ServiceFilterMessengerService
   ) { }
@@ -53,102 +23,60 @@ export class QuantityFilterComponentComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  setRangeOperatorAndValue(rangeOperatorAndValue : string, qtyInputEle : HTMLInputElement, index : number) {
-    this.rangeOperatorAndValue = `,${rangeOperatorAndValue} AOA${qtyInputEle.value}`;
-    this.qtyFilterInputs[index].value = qtyInputEle.value;
-    this.qtyFilterInputs[index].isActive = true;
-    this.disableAllQtyInputBut(index);
-    this.setFilter();
-
-    if (qtyInputEle.value == '' && index == 0) this.setInputFiltersToDefault();
-    if (qtyInputEle.value == '' && index > 0) this.setInputFilterToDefault(qtyInputEle, index);
-
-    this.checkCorrectInterval();
+  // set filter direction
+  moneyIn() {
+    this.filter.direction = 'Entradas';
   }
 
-  setTransactionDirection(direction : string) {
-    this.transactionDirection = direction;
-    this.setFilter();
+  moneyOut() {
+    this.filter.direction = 'Saídas';
   }
 
-  disableAllQtyInputBut(indx : number) {
-
-    /*
-    * disable only the equal input filter when either 
-    * at-least or no-more-than value filters are active
-    */
-    if (indx == 0) {
-      this.qtyFilterInputs.forEach((filter, index)=>{
-        if (index != indx) {
-          filter.isDisabled = true;
-        }
-      })  
-    } else this.qtyFilterInputs[0].isDisabled = true;
+  moneyInAndOut() {
+    this.filter.direction = '';
   }
 
-  setInputFiltersToDefault(equalQty? : HTMLInputElement,atLeastQty? : HTMLInputElement,noMoreThanQty? : HTMLInputElement) {
-    this.qtyFilterInputs.forEach(filter => filter.isDisabled = false);
-    this.qtyFilterInputs.forEach(filter => filter.value = '');
-
-    equalQty!.value = '';
-    atLeastQty!.value = '';
-    noMoreThanQty!.value = '';
+  // set filter value
+  specificAmount(value : string) {
+    this.filter.value = `,=AOA${value}`;
   }
 
-  setInputFilterToDefault(inputEle : HTMLInputElement, inputEleIndx : number) {
-    inputEle.value = '';
-    this.qtyFilterInputs[inputEleIndx].value = '';
-    if (this.areAllInputEleEmpty()) this.setInputFiltersToDefault();
-
-    this.removeErrorsFromInputElemnts();
+  atLeast(value : string) {
+    this.setValue(value, () => this.filter.value = `,>AOA${value}`)
   }
 
-  setFilter() {
-    this.serviceFilterMessengerService.filterDataToSend.next(
-      {
-        name : `${this.transactionDirection} ${this.rangeOperatorAndValue ? this.rangeOperatorAndValue : ''}`,
-        type : 'quantity'
-      }
-    );
+  noMoreThan(value : string) {
+    this.setValue(value, () => this.filter.value = `,<AOA${value}`);
   }
 
-  /*
-  * HELPERS *************************
-  */
-
-  areAllInputEleEmpty() : boolean {
-    let countTrue = 0;
-    this.qtyFilterInputs.forEach((input) => {
-      if (input.value == '') countTrue++;
-    });
-    return countTrue == 3;
+  // removes filter value
+  removesValue() {
+    this.filter.value = '';
   }
 
-  removeErrorsFromInputElemnts() {
-    this.qtyFilterInputs[1].error = false;
-    this.qtyFilterInputs[1].errorMsg = '';
-
-    this.qtyFilterInputs[2].error = false;
-    this.qtyFilterInputs[2].errorMsg = '';
+  // helper method to block input element
+  doesValueContain(operator : string) : boolean {
+    return this.filter.value.indexOf(operator) != -1;
   }
 
-  checkCorrectInterval() {
-    if (this.qtyFilterInputs[1].value != '' && this.qtyFilterInputs[2].value != '') {
-      if (+this.qtyFilterInputs[1].value > +this.qtyFilterInputs[2].value) {
-        this.qtyFilterInputs[1].error = true; 
-        this.qtyFilterInputs[2].error = true;
-  
-        this.qtyFilterInputs[1].errorMsg = 'Deve ser menor que o valor de \'Não mais que...\''; 
-        this.qtyFilterInputs[2].errorMsg = 'Deve ser maior que o valor de \'Pelo menos de....\'';
+  // the basic logic behind set value for the filter
+  setValue(value : string, cb : Function) {
+    if (this.filter.value == '') {
+      cb();
+    } else {
+      let firstValue = this.filter.value.slice(5), secondValue = value;
+      let operator = firstValue.split('')[1]; // helps us determine whether 1st value is < ou >.
+
+      if (operator == '>') {
+        if (+firstValue > +secondValue) this.isThereAnIntervalError = true;
+        else this.filter.value = `,AOA${firstValue}-AOA${secondValue}`; 
       } else {
-        this.qtyFilterInputs[1].error = false; 
-        this.qtyFilterInputs[2].error = false;
-  
-        this.qtyFilterInputs[1].errorMsg = ''; 
-        this.qtyFilterInputs[2].errorMsg = '';
+        if (+firstValue < +secondValue) this.isThereAnIntervalError = true;
+        else this.filter.value = `,AOA${firstValue}-AOA${secondValue}`; 
       }
     }
   }
+
 
 
 }
