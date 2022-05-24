68fc743d8cf12bfaead20db75baf5f623d8a9755
Luís Tchitue <ls04af@gmail.com>
2022-05-24 13:57:25 +0100

    adds slider functionality on the calendar to select date intervals

--- src/app/reports/auxiliary-components/filter-window/auxiliary-components/date-filter-component/date-filter-component.component.ts
+++ src/app/reports/auxiliary-components/filter-window/auxiliary-components/date-filter-component/date-filter-component.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceFilterMessengerService } from 'src/app/reports/service-messengers/service-filter-messenger.service';
+import { DateService, month } from './services/date.service';
+
+interface dateCustomType {
+  name : string
+}
+
+interface filterResult {
+  name : string,
+  type : string
+}
 
 @Component({
   selector: 'app-date-filter-component',
@@ -8,11 +18,59 @@ import { ServiceFilterMessengerService } from 'src/app/reports/service-messenger
 })
 export class DateFilterComponentComponent implements OnInit {
 
+  activeOption : string = 'Todas as transações';
+  isSelectMenuOpen : boolean = false;
+  activeMonthIndex! : number;
+
+  public customDateOptions : Array<dateCustomType> = [
+    {name : 'Últimos 30 dias'},
+    {name : 'Este mês'},
+    {name : 'Mês passado'},
+    {name : 'Este ano'},
+    {name : 'Ano passado'},
+    {name : 'Todas as transações'}
+  ]
+
+  public monthsObj : Array<month> = [];
+
   constructor(
-    private serviceFilterMessengerService : ServiceFilterMessengerService
+    private serviceFilterMessengerService : ServiceFilterMessengerService,
+    private dateService : DateService
   ) { }
 
-  ngOnInit(): void {
+  setCustomOption(optionName : string) {
+    this.activeOption = optionName;
+    this.setFilter({name : optionName, type : 'Date'});
   }
+
+  ngOnInit(): void {  
+    this.monthsObj = this.dateService.getMonths();
+  }
+
+  setFilter(filter : filterResult) {
+    this.serviceFilterMessengerService.filterDataToSend.next(filter);
+  }
+
+  toggleSelectMenuBox() {
+    this.isSelectMenuOpen = !this.isSelectMenuOpen;
+  }
+
+  activeMonth(month : month) {
+    this.dateService.deActivateAllMonths(this.monthsObj);
+    this.activeMonthIndex = month.index;
+    month.active = true;
+    
+  }
+
   
+  selectMonths(month : month) {
+    this.dateService.deSelectMonths(this.monthsObj);
+    if (month.index > 0) {
+      for (let i = this.activeMonthIndex; i < month.index; i++) {
+        if (i != this.activeMonthIndex)
+          this.monthsObj[i].selected = true;
+      }
+    }
+  }
+
 }
--- src/app/reports/auxiliary-components/filter-window/auxiliary-components/date-filter-component/services/date.service.ts
+++ src/app/reports/auxiliary-components/filter-window/auxiliary-components/date-filter-component/services/date.service.ts
@@ -1,9 +1,143 @@
 import { Injectable } from '@angular/core';
 
+export interface month  {
+  name : string,
+  numberOfDays : string,
+  year : number,
+  numberOfClicks : number,
+  selected : boolean,
+  index : number,
+  active : boolean
+} 
+
 @Injectable({
   providedIn: 'root'
 })
 export class DateService {
+  
+  public date;
 
-  constructor() { }
+  public months : Array<month> = [
+    {
+      name : 'Jan', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 0,
+      active : false
+    },
+    {
+      name : 'Fev', numberOfDays : '28',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 1,
+      active : false
+    },
+    {
+      name : 'Mar', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 2,
+      active : false
+    },
+    {
+      name : 'Abr', numberOfDays : '30',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 3,
+      active : false
+    },
+    {
+      name : 'Mai', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 4,
+      active : false
+    },
+    {
+      name : 'Jun', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 5,
+      active : false
+    },
+    {
+      name : 'Jul', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 6,
+      active : false
+    },
+    {
+      name : 'Ago', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 7,
+      active : false
+    },
+    {
+      name : 'Set', numberOfDays : '30',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 8,
+      active : false
+    },
+    {
+      name : 'Out', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 9,
+      active : false
+    },
+    {
+      name : 'Nov', numberOfDays : '30',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 10,
+      active : false
+    },
+    {
+      name : 'Dez', numberOfDays : '31',
+      year : 2022,
+      numberOfClicks : 0,
+      selected : false,
+      index : 11,
+      active : false
+    },
+  ];
+
+  constructor() {
+    this.date = new Date();
+  }
+
+  getMonths() : Array<month>{
+    return this.months;
+  }
+
+  deActivateAllMonths(months : Array<month>) {
+    months.forEach(month => month.active = false);
+  }
+
+  isThereAMonthActive(months : Array<month>) : boolean {
+    return months.filter(month => month.active == true).length > 0;
+  }
+
+  deSelectMonths(months : Array<month>) {
+    months.forEach(month => month.selected = false);
+  }
+
+  
+
+
+
 }
